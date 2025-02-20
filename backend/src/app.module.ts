fb1d0f7dc0ab881dbda3fdc3969c0e807a7de2cb
Petra Popovic <ppopovic0411@gmail.com>
2025-02-20 23:44:25 +0100

    zavrsen setup projekta

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      password: 'postgres',
+      username: 'postgres',
+      host: 'iteh-projekat-postgres',
+      database: 'iteh-projekat',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
