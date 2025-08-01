71f91291714f37ea7c4ed809bd4d3e178d0d248d
Petra Popovic <ppopovic0411@gmail.com>
2025-08-01 23:57:30 +0200

    css za login

--- frontend/src/pages/OdgovornoLice/OdgovornoLicePrikaz.tsx
+++ frontend/src/pages/OdgovornoLice/OdgovornoLicePrikaz.tsx
@@ -1,6 +1,6 @@
 import LogoutDugme from '../../components/LogoutDugme/LogoutDugme';
 import ProfesorMikrokredencijali from '../../components/Mikrokredencijal/ProfesorMikrokredencijal';
-
+import styles from './OdgovornoLice.module.css';
 interface Korisnik {
   id: number;
   email: string;
@@ -13,10 +13,10 @@ interface OdgovornoLicePrikazProps {
 
 function OdgovornoLicePrikaz({ user }: OdgovornoLicePrikazProps) {
   return (
-    <>
+    <div className={styles.proba}>
       <ProfesorMikrokredencijali id={user.id} />
       <LogoutDugme />
-    </>
+    </div>
   );
 }
 export default OdgovornoLicePrikaz;
